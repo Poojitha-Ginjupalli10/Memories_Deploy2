db52064bc28fbff3b85f974d77d64712fc40f18c
Poojitha-Ginjupalli10 <poojithaginjupalli@gmail.com>
2023-10-13 09:56:55 +0530

    Updates

---
+++ precache-manifest.17dd7de101a3a8c8d01b374055a48922.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7efddb5d9b94693f1508ab362ca4830",
+    "url": "/Memories_Deploy2/index.html"
+  },
+  {
+    "revision": "85a601250a59500d08e2",
+    "url": "/Memories_Deploy2/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "010b5d226f8e0eb3ad48",
+    "url": "/Memories_Deploy2/static/js/2.8a522347.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/Memories_Deploy2/static/js/2.8a522347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a601250a59500d08e2",
+    "url": "/Memories_Deploy2/static/js/main.e66abdf6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Memories_Deploy2/static/js/main.e66abdf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f83e0d6eb90f3660e0",
+    "url": "/Memories_Deploy2/static/js/runtime-main.049e0f59.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/Memories_Deploy2/static/media/memories.5c0c63fb.png"
+  }
+]);
